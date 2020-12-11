706ceb08fc7d107b7056c5eb9b61693d67a4d671
murphy <987127314@qq.com>
2020-12-11 16:12:28 +0800

    将记账页面的HTML和CSS写好并模块化

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="add"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -22,9 +22,10 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
   nav{
+    @extend %outerShadow;
     display: flex;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5);
     flex-direction: row;
     font-size: 12px;
     > .item{
@@ -34,6 +35,9 @@
       justify-content: center;
       align-items: center;
       flex-direction: column;
+    }
+    > .item.selected{
+      color: $color-highlight;
     }
   }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,16 +1,26 @@
 <template>
-  <div>
-    <Layout>
-      <p>money</p>
-    </Layout>
-  </div>
+  <Layout classPrefix="layout">
+    <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags/>
+  </Layout>
 </template>
 
 <script lang="ts">
+  import NumberPad from '@/components/Money/NumberPad.vue';
+  import Types from '@/components/Money/Types.vue';
+  import Notes from '@/components/Money/Notes.vue';
+  import Tags from '@/components/Money/Tags.vue';
   export default {
     name: 'money',
+    components: {Tags, Notes, Types, NumberPad},
   };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+  .layout-content {
+    display: flex;
+    flex-direction: column-reverse;
+  }
 </style>
